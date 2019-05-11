c4506a9cbe3c41e0e455561689afd67c08a022a2
Thiago Alves <thiago_marketingdigital@hotmail.com>
2019-05-11 20:42:26 -0300

    Update 1.5
    MELHORIAS
    # Permite fazer uma nova operação.
    # Limpeza do código - Fácil manutenção.
    # Remoção de comentários (linhas de códigos auto-explicativos).

--- binarios.c
+++ binarios.c
@@ -1,71 +1,128 @@
 /* 
-
 Thiago Alves | Curso de Engenharia de Software | Faculdade Uniamérica - 2019/2023.
 
-Conversor de numero decimal para binário utilizando a lógica do par ou impar.
-
-Como todos provavelmente devem saber, todo número par é divido por 2, ou seja, caso o
-resultado da divisão não tenha restos então o par é verdadeiro "1" caso contrário é falso "0".
-
-Para chegar ao resultado do número binário deve se fazer a divisão do número fornecido pelo 
-usuário e verificar se o resto da divisão irá retornar verdadeiro(1) ou falso(0). 
-Esse processo será repetido e armazenado em um vetor até que o resultado da ultima divisão 
-chegue no valor 1 ou 0 e não dê mais para dividir. Então imprimimos na tela o resultado e voilà.
+Conversor de Número decimal para binário.
 
 MELHORIAS
 # Menu de opção
 # Converte o numero de decimal para binário.
 # converte o numero binário para decimal.
+# Contador de Bits
+# Limpeza do código - Fácil manutenção.
+# Remoção de comentários (linhas de códigos auto-explicativos).
 */
 
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 
-void convert_to_binario();
-int convert_to_decimal();
-void inverter_string();
+int menu_principal();
 void digite_numero();
 void imprima();
+void inverter_string();
+void convert_to_binario();
+int convert_to_decimal();
 
-
-char string[127] = {'0'}; // irá armazenar o número em string
+char string[127]; // irá armazenar o número em string
 int numero = 0; 
 int contador_bits = 0;
 
 int main() {
 
-	int opcao;
-	printf("\n=== Escolha com Sabedoria ===\n");
-	printf("[1]Converter decimal para binario\n");
-	printf("[2]Converter binario para decimal\n");
-	printf("[?]Sair\n");
-	printf("\nDigite: ");
-	scanf("%d", &opcao);
+	int opcao = 0;
 
-	if (opcao == 1) {	
-		digite_numero(opcao);
-		convert_to_binario(numero);
-		imprima(opcao);
-	} else if (opcao == 2){
-		//resolução numero decimal para binario
-		digite_numero(opcao);
-		numero = convert_to_decimal();
-		imprima(opcao);
-	}
+	do	{
 
-	else
-		printf("\nSaindo do aplicativo...");
+		opcao = menu_principal(opcao);
+
+		if (opcao == 1) {	
+			digite_numero(opcao);
+			convert_to_binario(numero);
+			imprima(opcao);
+		} else if (opcao == 2){
+			//resolução numero decimal para binario
+			digite_numero(opcao);
+			numero = convert_to_decimal(numero);
+			imprima(opcao);
+		}
+
+		else
+			printf("\nSaindo do aplicativo...");
+
+	} while(opcao == 1 || opcao == 2);
 
 	return 0;
 }
 
+int menu_principal(int opcao){
+
+	//reset value
+	sprintf(string, "0");
+	opcao = 0;
+
+	printf("\n=== Escolha o tipo de conversao ===\n");
+	printf("[1]Decimal para binario\n");
+	printf("[2]Binario para decimal\n");
+	printf("[?]Sair\n");
+	printf("\n$ ");
+	
+	scanf("%d", &opcao);
+	fflush(stdin);
+
+	return opcao;//Apenas mostra o menu principal
+}
+
+void digite_numero(int opcao){
+	
+	printf("\nDigite um numero: ");
+	scanf("%d", &numero);
+
+	if (opcao == 2) {
+		sprintf(string, "%d", numero);
+		inverter_string();
+	}
+
+	//DEBUG printf("\nInteiro: %d \nString: %s \n", numero, string);
+	//Em breve - validação de dados e anti buffer
+}
+
+void imprima(int opcao){ //mostra o resultado de acordo com a opção escolhida.
+	
+	if (opcao == 1) { 
+		printf("\nValor em binario: %s", string);
+	} else
+		printf("\nValor em decimal: %d", numero);
+
+	printf("\nTamanho: %d bit", contador_bits);
+	if(contador_bits > 1)
+		printf("s\n");	// Mero capricho
+}
+
+void inverter_string() {
+
+	char reverse[127] = {'0'};
+	int novaPosicao = 0;
+	
+	for (int antigaPosicao = strlen(string); antigaPosicao >= 0; antigaPosicao--) 
+	{
+		if(string[antigaPosicao] == '0' || string[antigaPosicao] == '1')
+		{ 
+			reverse[novaPosicao] = string[antigaPosicao];
+			novaPosicao++;
+		}
+	}
+
+	strcpy(string, reverse);//Apenas inverte ao contrário o numero binário.
+}
+
 void convert_to_binario(int numeroDecimal) { 
 
-	const int BITLIMIT = 31;  //limitador - configurado para processar até 31^16
+	const int BITLIMIT = 64;  //limitador
 
 	for (int posicao = 0; posicao <= BITLIMIT; posicao++)
 	{		
+		contador_bits = posicao + 1;
+
 		if (numeroDecimal == 1)	{ 
 			string[posicao] = '1'; 
 			break; 
@@ -85,7 +142,6 @@ void convert_to_binario(int numeroDecimal) {
 				string[posicao] = '0'; 
 		}
 
-		contador_bits = posicao + 1;
 		numeroDecimal /= 2; 
 	}
 
@@ -94,16 +150,14 @@ void convert_to_binario(int numeroDecimal) {
 
 int convert_to_decimal() {
 	
-	inverter_string();
 	int posicao = strlen(string) - 1; //Essencial para a precisão do resultado.
-
 	int Numero_decimal = 0; 
 
-	for(int N_do_expoente = posicao; N_do_expoente >= 0; N_do_expoente--) {
+	for(int Numero_expoente = posicao; Numero_expoente >= 0; Numero_expoente--) {
 
-		if (string[N_do_expoente] != '0'){
+		if (string[Numero_expoente] != '0'){
 			//DEBUG printf("\nNa casa %d o resultado sera %c * 2^%d", N_do_expoente, aux[string], N_do_expoente);
-			int valor_do_bit = pow(2, N_do_expoente);
+			int valor_do_bit = pow(2, Numero_expoente);
 			Numero_decimal += valor_do_bit;
 			//DEBUG printf("\nO resultado e': %d", Numero_decimal);
 		}
@@ -111,46 +165,4 @@ int convert_to_decimal() {
 
 	contador_bits = strlen(string);
 	return Numero_decimal; //retorna o resultado da operacao
-}
-
-void inverter_string() {
-
-	char reverse[127] = {'0'};
-	int novaPosicao = 0;
-	
-	for (int antigaPosicao = strlen(string); antigaPosicao >= 0; antigaPosicao--) 
-	{
-		if(string[antigaPosicao] == '0' || string[antigaPosicao] == '1')
-		{ 
-			reverse[novaPosicao] = string[antigaPosicao];
-			novaPosicao++;
-		}
-	}
-
-	strcpy(string, reverse);
-}
-
-void digite_numero(int opcao){
-	
-	printf("\nDigite um numero: ");
-	scanf("%d", &numero);
-
-	if (opcao == 2)
-		sprintf(string, "%d", numero);
-
-	//DEBUG printf("\nInteiro: %d \nString: %s \n", numero, string);
-	//Em breve - validação de dados e anti buffer
-	
-}
-
-void imprima(int opcao){ //mostra o resultado de acordo com a opção escolhida.
-	
-	if (opcao == 1) { 
-		printf("\nValor em binario: %s", string);
-	} else
-		printf("\nValor em decimal: %d", numero);
-
-	printf("\nTamanho: %d bit", contador_bits);
-	if(contador_bits > 1)
-		printf("s\n");	// Mero capricho
 }
